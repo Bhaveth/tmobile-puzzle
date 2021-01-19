7a397e3041a887bb6e9dc6f37328c69db177884e
Bhaveth <bhavethvarma123@gmail.com>
2021-01-18 23:17:14 -0500

    Undo add and remove from reading list

--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import {
   addToReadingList,
   clearSearch,
   getAllBooks,
   ReadingListBook,
-  searchBooks
+  searchBooks,
+  removeFromReadingList,
+  confirmedAddToReadingList
 } from '@tmo/books/data-access';
 import { FormBuilder } from '@angular/forms';
 import { Book } from '@tmo/shared/models';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Actions, ofType } from '@ngrx/effects';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'tmo-book-search',
   templateUrl: './book-search.component.html',
   styleUrls: ['./book-search.component.scss']
 })
-export class BookSearchComponent implements OnInit {
+export class BookSearchComponent implements OnInit, OnDestroy {
+  destroyed$ = new Subject<boolean>();
   books: ReadingListBook[];
 
   searchForm = this.fb.group({
@@ -24,8 +31,18 @@ export class BookSearchComponent implements OnInit {
 
   constructor(
     private readonly store: Store,
-    private readonly fb: FormBuilder
-  ) {}
+    private readonly fb: FormBuilder,
+    private _snackBar: MatSnackBar,
+    private action$: Actions
+  ) {
+    action$.pipe(
+      ofType(confirmedAddToReadingList),
+      takeUntil(this.destroyed$)
+   )
+   .subscribe((data) => {
+      this.openSnackBar(data.book);
+   });
+  }
 
   get searchTerm(): string {
     return this.searchForm.value.term;
@@ -58,5 +75,20 @@ export class BookSearchComponent implements OnInit {
     } else {
       this.store.dispatch(clearSearch());
     }
+  }
+
+  openSnackBar(book: Book) {
+    let snackBarRef = this._snackBar.open('Book added to reading list', 'Undo', {
+      duration: 2000,
+    });
+
+    snackBarRef.onAction().subscribe(() => {
+      const item = { bookId: book.id, ...book }
+      this.store.dispatch(removeFromReadingList({ item }));
+    });
+  }
+  ngOnDestroy(): void {
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
   }
 }
--- libs/books/feature/src/lib/reading-list/reading-list.component.ts
+++ libs/books/feature/src/lib/reading-list/reading-list.component.ts
@@ -1,18 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
+import { getReadingList, removeFromReadingList, addToReadingList, confirmedRemoveFromReadingList } from '@tmo/books/data-access';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import { ReadingListItem } from '@tmo/shared/models';
+import { Actions, ofType } from '@ngrx/effects';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'tmo-reading-list',
   templateUrl: './reading-list.component.html',
   styleUrls: ['./reading-list.component.scss']
 })
-export class ReadingListComponent {
+export class ReadingListComponent implements OnDestroy{
   readingList$ = this.store.select(getReadingList);
+  destroyed$ = new Subject<boolean>();
 
-  constructor(private readonly store: Store) {}
+  constructor(private readonly store: Store,  private _snackBar: MatSnackBar, private action$: Actions) {
+    action$.pipe(
+      ofType(confirmedRemoveFromReadingList),
+      takeUntil(this.destroyed$)
+   )
+   .subscribe((data) => {
+      this.openSnackBar(data.item);
+   });
+  }
 
   removeFromReadingList(item) {
     this.store.dispatch(removeFromReadingList({ item }));
+  }
+
+  openSnackBar(item: ReadingListItem) {
+    let snackBarRef = this._snackBar.open('Book removed from reading list', 'Undo', {
+      duration: 2000,
+    });
+
+    snackBarRef.onAction().subscribe(() => {
+      const book = {id: item.bookId, ...item};
+      this.store.dispatch(addToReadingList({ book }));
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
   }
 }
