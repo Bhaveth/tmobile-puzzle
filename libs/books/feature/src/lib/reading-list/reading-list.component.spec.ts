27c389518362149f408d41a014d8520eef808d66
Bhaveth <bhavethvarma123@gmail.com>
2021-02-02 22:37:49 -0500

    mark as read e2e test cases

--- libs/books/feature/src/lib/reading-list/reading-list.component.spec.ts
+++ libs/books/feature/src/lib/reading-list/reading-list.component.spec.ts
@@ -1,26 +1,63 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { SharedTestingModule } from '@tmo/shared/testing';
+import { $, $$, browser, ExpectedConditions } from 'protractor';
 
-import { ReadingListComponent } from './reading-list.component';
-import { BooksFeatureModule } from '@tmo/books/feature';
+describe('When: I use the reading list feature', () => {
+  it('Then: I should see my reading list', async () => {
+    await browser.get('/');
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement($('tmo-root'), 'okreads')
+    );
 
-describe('ReadingListComponent', () => {
-  let component: ReadingListComponent;
-  let fixture: ComponentFixture<ReadingListComponent>;
+    const readingListToggle = await $('[data-testing="toggle-reading-list"]');
+    await readingListToggle.click();
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [BooksFeatureModule, SharedTestingModule]
-    }).compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ReadingListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement(
+        $('[data-testing="reading-list-container"]'),
+        'My Reading List'
+      )
+    );
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Then: should able to mark a book as read', async () => {
+    await browser.get('/');
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement($('tmo-root'), 'okreads')
+    );
+
+    const form = await $('form');
+    const input = await $('input[type="search"]');
+    await input.sendKeys('Java');
+    await form.submit();
+    const items = await $$('[data-testing="book-item"]');
+    expect(items.length).toBeGreaterThan(1);
+
+    const searchItems = await $$('[data-testing="book-item"] button');
+    searchItems[0].click();
+    const readingListClickToAdd = await $(
+      '[data-testing="toggle-reading-list"]'
+    );
+    await readingListClickToAdd.click();
+    const finishReadingButton = await $(
+      '[data-testing="mark-as-read"]'
+    );
+    await finishReadingButton.click();
+
+    const finishText = await $('[data-testing="complete-reading"]');
+
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement(
+        finishText,
+        'Completed On:'
+      )
+    );
+
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement(
+        finishText,
+        'Finished'
+      )
+    );
+    browser.sleep(200);
+    await $('[data-testing="remove-book-from-readinglist"]').click();
   });
 });
