941302c29e7c8533905a9405ee7cb2cc264c37da
Bhaveth <bhavethvarma123@gmail.com>
2021-02-02 22:23:07 -0500

    Undo actions e2e test case

--- libs/books/feature/src/lib/reading-list/reading-list.component.spec.ts
+++ libs/books/feature/src/lib/reading-list/reading-list.component.spec.ts
@@ -1,26 +1,74 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { SharedTestingModule } from '@tmo/shared/testing';
+import { $, $$, browser, ExpectedConditions, by, element } from 'protractor';
 
-import { ReadingListComponent } from './reading-list.component';
-import { BooksFeatureModule } from '@tmo/books/feature';
+describe('When: I use the reading list feature', () => {
+  it('Then: I should see my reading list', async () => {
+    await browser.get('/');
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement($('tmo-root'), 'okreads')
+    );
 
-describe('ReadingListComponent', () => {
-  let component: ReadingListComponent;
-  let fixture: ComponentFixture<ReadingListComponent>;
+    const readingListToggle = await $('[data-testing="toggle-reading-list"]');
+    await readingListToggle.click();
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [BooksFeatureModule, SharedTestingModule]
-    }).compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ReadingListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement(
+        $('[data-testing="reading-list-container"]'),
+        'My Reading List'
+      )
+    );
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Then: I should see my reading list and snackbar after adding book to Reading List', async () => {
+    await browser.get('/');
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement($('tmo-root'), 'okreads')
+    );
+    const form = await $('form');
+    const input = await $('input[type="search"]');
+    await input.sendKeys('java');
+    await form.submit();
+    const readingListBooks = await $$('[data-testing="book-item"]');
+    expect(readingListBooks.length).toBeGreaterThan(1);
+
+    await $$('.book--content--info button:enabled').first().click();
+
+    const readingListClickToggle = await $(
+      '[data-testing="toggle-reading-list"]'
+    );
+    await readingListClickToggle.click();
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement(
+        $('[data-testing="reading-list-container"]'),
+        'My Reading List'
+      )
+    );
+    browser.sleep(2000);
+  });
+
+  it('Then: I should see my reading list and undo action of reading list items', async () => {
+    await browser.get('/');
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement($('tmo-root'), 'okreads')
+    );
+    const readingListClickToggle = await $(
+      '[data-testing="toggle-reading-list"]'
+    );
+    await readingListClickToggle.click();
+    let readingListitems = await $$('[data-testing="reading-book-list-item"]');
+    const readingListitemsLength = readingListitems.length;
+    const removeFromReadingListButton = await $(
+      '[data-testing="remove-reading-list-button"]'
+    );
+    await removeFromReadingListButton.click();
+    await (
+      await browser.driver.findElement(
+        by.tagName('.mat-simple-snackbar-action')
+      )
+    ).click();
+    readingListitems = await $$('[data-testing="reading-book-list-item"]');
+    expect(readingListitems.length).toEqual(readingListitemsLength);
+    browser.sleep(2000);
+    await removeFromReadingListButton.click();
+    browser.sleep(1000);
   });
 });
